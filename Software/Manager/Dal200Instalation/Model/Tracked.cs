efc5f52726695f8c3ead21c17df07fa342eeaa69
Juliano Franz <jmfranz@users.noreply.github.com>
2018-03-19 11:34:30 -0300

    Added fake dwell informatio every 30s

--- Software/Manager/Dal200Instalation/Model/Tracked.cs
+++ Software/Manager/Dal200Instalation/Model/Tracked.cs
@@ -10,11 +10,21 @@ namespace Dal200Instalation.Model
     {
         public int id { get; }
         public int[] position;
+        public string track;
+        public string mediaName;
 
         public Tracked(int id, int x, int y)
         {
             this.id = id;
             position = new[] {x, y};
+        }
+
+        public Tracked(int id, int x, int y, string track, string mediaName)
+        {
+            this.id = id;
+            position = new[] { x, y };
+            this.track = track;
+            this.mediaName = mediaName;
         }
     }
 
