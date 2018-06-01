ff0528f71cd0f1efa0bbeef88c0b960d50ccb77e
Joseph Malloch <joseph.malloch@gmail.com>
2018-06-01 17:29:52 -0300

    Working on floor display.

--- Software/SVGRenderer/SVGRenderer.js
+++ Software/SVGRenderer/SVGRenderer.js
@@ -24,7 +24,7 @@ function init() {
     cx = width * 0.5;
     cy = height * 0.5;
 
-    identify();
+//    identify();
 
     if (navigator.onLine) {
         console.log("You are Online");
@@ -33,7 +33,7 @@ function init() {
     }
 
     // Create a new WebSocket.
-    socket = new WebSocket('ws://134.190.155.223/Dal200');
+    socket = new WebSocket('ws://192.168.1.120/Dal200');
 //    socket = new WebSocket('ws://192.168.0.107/Dal200');
     socket.onopen = function(event) {
         console.log("Connection established");
@@ -48,20 +48,25 @@ function init() {
             return;
 //        console.log(data);
         if (data.trackerData) {
+//            console.log('trackerData:', data.trackerData);
             for (var i in data.trackerData) {
                 updateTrackerData(data.trackerData[i].id,
-                                  data.trackerData[i].position);
+                                  convertCoords(data.trackerData[i].position));
+//                console.log(data.trackerData[i].position,
+//                            convertCoords(data.trackerData[i].position));
             }
         }
         else if (data.targets) {
-            console.log(data);
+//            console.log('targets:', data.targets);
             for (var i in data.targets) {
-                updateTarget(data.targets[i].UUID, data.targets[i].Position,
-                             data.targets[i].Label);
+//                console.log('target:', data.targets[i].UUID, data.targets[i].Position,
+//                            convertCoords(data.targets[i].Position));
+                updateTarget(data.targets[i].UUID, convertCoords(data.targets[i].Position),
+                             data.targets[i].Label, data.targets[i].Type);
             }
         }
         else if (data.paths) {
-            console.log('paths:', data.paths);
+//            console.log('paths:', data.paths);
             for (var i in data.paths) {
                 updatePath(data.paths[i].UUID, data.paths[i].source,
                            data.paths[i].destination);
@@ -94,11 +99,58 @@ function init() {
         }
     })
 
-        // debugging: add a couple of targets
-    updateTarget(0, randomCoord(), "category 1");
-    updateTarget(1, randomCoord(), "category 2");
+    // debugging: add a couple of targets
+//    updateTarget(0, randomCoord(), "category 1", 1);
+//    updateTarget(1, randomCoord(), "category 2", 0);
+//
+//    updatePath(3, 0, 1);
 
-    updatePath(3, 0, 1);
+//    function makeSteps(num) {
+//        let steps = [];
+//        for (var i = 0; i < num; i++) {
+//            let path = canvas.path([['M', 0, 0],
+//                                    ['l', 0, 500]])
+//                             .attr({'stroke-width': 30,
+//                                    'stroke': 'white'})
+//                             .rotate(-5)
+//                             .translate(220 + i * 40, 165);
+//            steps.push(path);
+//        }
+//    }
+//
+//    makeSteps(9);
+
+//    // step 1
+//    canvas.path([['M', 0, 0],
+//                 ['l', 0, 500]])
+//          .attr({'stroke-width': 30,
+//                 'stroke': Raphael.getColor()})
+//          .rotate(-5)
+//          .translate(220, 165);
+//
+//    canvas.circle(300, 500, 40).attr({'fill': 'white',
+//                                      'fill-opacity': 1,
+//                                      'stroke-width': 10,
+//                                      'opacity': 1});
+}
+
+function convertCoords(pos) {
+    let offset = {'x': 10, 'y': -250};
+    let scale = {'x': 2.25, 'y': 2.5};
+//    let offset = {'x': 400, 'y': -320};
+//    let scale = {'x': 2.4, 'y': 2.5};
+//    let offset = {'x': -1110, 'y': -340};
+//    let scale = {'x': 1.68, 'y': 2.9};
+
+    let x = pos.x * scale.x + offset.x;
+    let y = pos.y * scale.y + offset.y;
+
+    x = 512 - x + 750;
+
+    if (pos.x > 465)
+        x -= 45;
+
+    return {'x': x, 'y': y};
 }
 
 function identify() {
@@ -132,6 +184,7 @@ function distSquared(pos1, pos2) {
 }
 
 function updateTrackerData(id, pos) {
+//    pos.x = 512 - pos.x + 750;
     if (!trackerData[id]) {
         trackerData[id] = canvas.circle(pos.x, pos.y, 40)
                                 .attr({'stroke': 'white',
@@ -142,7 +195,10 @@ function updateTrackerData(id, pos) {
         trackerData[id].animationFrame = 0;
     }
     trackerData[id].stop();
-    trackerData[id].animate({'r': trackerData[id].animationFrame, 'opacity': 1},
+//    console.log('placing tracker', id, 'at', pos);
+    trackerData[id].animate({'cx': pos.x,
+                             'cy': pos.y,
+                             'r': trackerData[id].animationFrame, 'opacity': 1},
                            300, 'linear', function() {
         this.animate({'opacity': 0}, 10000, '>', function() {
             trackerData[this.data('id')] = null;
@@ -153,36 +209,52 @@ function updateTrackerData(id, pos) {
         trackerData[id].animationFrame = 20;
 
     // check if we are close to any targets
-    for (var i in targets) {
-        let dist = distSquared(pos, targets[i].data('pos'));
-//        console.log(id, i, dist);
-        if (dist < distThresh) {
-            console.log('tracker', id, 'proximate to target', i);
-            if (targets[i].sel < 255)
-                targets[i].sel++;
-        }
-        else if (targets[i].sel > 0)
-            targets[i].sel--;
-        let color = targets[i].sel;
-        targets[i].attr({'fill': Raphael.rgb(255, 255-color, 255-color)});
-    }
+//    for (var i in targets) {
+//        let dist = distSquared(pos, targets[i].data('pos'));
+////        console.log(id, i, dist);
+//        if (dist < distThresh) {
+////            console.log('tracker', id, 'proximate to target', i);
+//            if (targets[i].sel < 255)
+//                targets[i].sel++;
+//        }
+//        else if (targets[i].sel > 0)
+//            targets[i].sel--;
+//        let color = targets[i].sel;
+//        targets[i].attr({'fill': Raphael.rgb(255, 255-color, 255-color)});
+//    }
 }
 
-function updateTarget(id, pos, label) {
+function updateTarget(id, pos, label, type) {
     if (!targets[id]) {
-        targets[id] = canvas.path([['M', pos.x, pos.y]]);
+        targets[id] = canvas.circle(0, 0, 30 + (pos.x) * 0.04);
         targets[id].label = canvas.text(pos.x, pos.y, label)
-                                  .rotate(-90);
+                                  .rotate(90);
         targets[id].sel = 0;
     }
-    targets[id].pos = pos;
+//    console.log('placing target', id, 'at', pos);
+    targets[id].attr({'cx': pos.x, 'cy': pos.y});
     targets[id].data({'pos': pos});
-    targets[id].animate({'path': circlePath(pos, 50),
-                         'fill': 'white'});
+    let color;
+    switch (type) {
+        case 0:
+            color = '#FFBBBB';
+            break;
+        case 1:
+            color = '#BBFFBB';
+            break;
+        case 2:
+            color = '#BBBBFF';
+            break;
+        default:
+            color = '#FFFFFF';
+            break;
+    }
+
+    targets[id].animate({'fill': color});
     targets[id].label.animate({'x': pos.x,
-                              'y': pos.y,
-                              'stroke': 'black',
-                              'font-size': 16});
+                               'y': pos.y,
+                               'stroke': 'black',
+                               'font-size': 16});
 //    console.log(targets);
 }
 
