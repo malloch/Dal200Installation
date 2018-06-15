14aa791be165fdea33dda1e6b1bfae53fe4c91b7
Juliano Franz <jmfranz@users.noreply.github.com>
2018-06-15 19:34:19 -0300

    Message list overflow fix + NBug reports

--- Software/Manager/Dal200Instalation/App.xaml.cs
+++ Software/Manager/Dal200Instalation/App.xaml.cs
@@ -14,6 +14,20 @@ namespace Dal200Instalation
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            // Uncomment the following after testing to see that NBug is working as configured
+            NBug.Settings.ReleaseMode = true;
+
+            // NBug configuration (you can also choose to create xml configuration file)
+            NBug.Settings.StoragePath = NBug.Enums.StoragePath.CurrentDirectory;
+            NBug.Settings.UIMode = NBug.Enums.UIMode.Full;
+
+            // Hook-up to all possible unhandled exception sources for WPF app, after NBug is configured
+            AppDomain.CurrentDomain.UnhandledException += NBug.Handler.UnhandledException;
+            Application.Current.DispatcherUnhandledException += NBug.Handler.DispatcherUnhandledException;
+        }
+
         private void Application_Startup(object sender, StartupEventArgs e)
         {
             var mainWindow = new MainWindow();
@@ -23,5 +37,7 @@ namespace Dal200Instalation
             var mainViewModel = new MainWindowViewModel();
             mainWindow.DataContext = mainViewModel;
         }
+
+
     }
 }
